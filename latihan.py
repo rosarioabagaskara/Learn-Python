515fbcf9c1dd0de00dd203a6b1aacc7e3cac84e6
Rosario Bagaskara <rosariobagaskara@gmail.com>
2022-11-05 22:19:46 +0700

    latihan

--- latihan.py
+++ latihan.py
@@ -1,16 +1,35 @@
 # binary to decimal
 
-input = 100
+input = 111
 
 
 def binaryToDecimal(input):
-    binaryValue = str(input)
-    result = 0
+    str_input = str(input)
     temp = 1
-    for i in range(len(binaryValue) - 1, -1, -1):
-        result += int(binaryValue[i]) * temp
-        temp = temp * 2
+    result = 0
+    for i in range(len(str_input)-1, -1, -1):
+        result += int(str_input[i]) * temp
+        temp *= 2
+
     return result
 
 
 print(binaryToDecimal(input))
+
+
+# Sort using bubble
+
+# array1 = [5, 1, 2, 4, 5]
+# array2 = [9, 2, 4, 5, 10, 20, 30]
+
+# merge = array1 + array2
+
+# for i in range(0, len(merge)):
+#     temp = 0
+#     for j in range(0, len(merge)-i-1):
+#         if (merge[j] > merge[j+1]):
+#             temp = merge[j]
+#             merge[j] = merge[j+1]
+#             merge[j+1] = temp
+
+# print(merge)
